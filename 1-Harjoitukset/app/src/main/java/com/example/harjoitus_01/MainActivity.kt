994d21840e4e50d5e21c1946533f94fe9554f265
Jyri Nieminen <nieminen.jyri.j@gmail.com>
2026-09-21 18:59:13 +0300

    3rd Assingment, part 1
    Modified the application's main view to use the Material 3 Scaffold component.

--- 1-Harjoitukset/app/src/main/java/com/example/harjoitus_01/MainActivity.kt
+++ 1-Harjoitukset/app/src/main/java/com/example/harjoitus_01/MainActivity.kt
@@ -12,11 +12,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,8 +50,43 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        modifier = modifier,
+
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Profiili")
+                }
+            )
+        },
+
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Lisää"
+                )
+            }
+        }
+    ) { innerPadding ->
+
+        ProfileContent(
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+
+@Composable
+fun ProfileContent(
     modifier: Modifier = Modifier
 ) {
     Column(

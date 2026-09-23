491adff14d4f4504cc45162c448615f17f13eb1b
Jyri Nieminen <nieminen.jyri.j@gmail.com>
2026-09-23 19:49:46 +0300

    5th Assingment, part 6
    Created an alarm notification to tell if the filter shows nothing

--- 1-Harjoitukset/app/src/main/java/com/example/harjoitus_01/TechnologyListScreen.kt
+++ 1-Harjoitukset/app/src/main/java/com/example/harjoitus_01/TechnologyListScreen.kt
@@ -4,27 +4,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Switch
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TechnologyListScreen (
+fun TechnologyListScreen(
     modifier: Modifier = Modifier
 ) {
     var showFavoritesOnly by rememberSaveable {
@@ -32,10 +32,13 @@ fun TechnologyListScreen (
     }
 
     val visibleTechnologies = if (showFavoritesOnly) {
-        ProgrammingTechnologies.filter { it.isFavorite }
-    }else{
+        ProgrammingTechnologies.filter {
+            it.isFavorite
+        }
+    } else {
         ProgrammingTechnologies
     }
+
     Scaffold(
         modifier = modifier,
         topBar = {
@@ -47,9 +50,7 @@ fun TechnologyListScreen (
                 }
             )
         }
-
-    ) {
-        innerPadding ->
+    ) { innerPadding ->
 
         LazyColumn(
             modifier = Modifier
@@ -83,16 +84,27 @@ fun TechnologyListScreen (
                 }
             }
 
-            items(
-                items = visibleTechnologies,
-                key = { technology ->
-                    technology.id
-                }
-            ) { technology ->
+            if (visibleTechnologies.isEmpty()) {
 
-                ProgrammingTechnologyCard(
-                    technology = technology
-                )
+                item {
+                    Text(
+                        text = "Suosikkeja ei löytynyt."
+                    )
+                }
+
+            } else {
+
+                items(
+                    items = visibleTechnologies,
+                    key = { technology ->
+                        technology.id
+                    }
+                ) { technology ->
+
+                    ProgrammingTechnologyCard(
+                        technology = technology
+                    )
+                }
             }
         }
     }

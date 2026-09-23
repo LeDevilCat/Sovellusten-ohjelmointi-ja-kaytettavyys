588d43b6d8a21befaa0c89764c41b2add536e418
Jyri Nieminen <nieminen.jyri.j@gmail.com>
2026-09-23 19:42:54 +0300

    5th Assingment, part 5
    Added a switch to filter between all items and favourited items

--- 1-Harjoitukset/app/src/main/java/com/example/harjoitus_01/TechnologyListScreen.kt
+++ 1-Harjoitukset/app/src/main/java/com/example/harjoitus_01/TechnologyListScreen.kt
@@ -11,14 +11,31 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TechnologyListScreen (
     modifier: Modifier = Modifier
 ) {
+    var showFavoritesOnly by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    val visibleTechnologies = if (showFavoritesOnly) {
+        ProgrammingTechnologies.filter { it.isFavorite }
+    }else{
+        ProgrammingTechnologies
+    }
     Scaffold(
         modifier = modifier,
         topBar = {
@@ -40,14 +57,39 @@ fun TechnologyListScreen (
                 .padding(innerPadding),
 
             contentPadding = PaddingValues(16.dp),
+
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
+
+            item {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 4.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Text(
+                        text = "Näytä vain suosikit"
+                    )
+
+                    Switch(
+                        checked = showFavoritesOnly,
+                        onCheckedChange = { checked ->
+                            showFavoritesOnly = checked
+                        }
+                    )
+                }
+            }
+
             items(
-                items = ProgrammingTechnologies,
+                items = visibleTechnologies,
                 key = { technology ->
                     technology.id
                 }
             ) { technology ->
+
                 ProgrammingTechnologyCard(
                     technology = technology
                 )

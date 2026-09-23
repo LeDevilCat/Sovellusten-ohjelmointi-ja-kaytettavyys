1c00f3b0af54a84b3242d835d208a6aa26b507bf
Jyri Nieminen <nieminen.jyri.j@gmail.com>
2026-09-23 18:39:32 +0300

    5th Assingment, part 1
    Created ProgrammingTechnology.kt and define a data class in it

---
+++ 1-Harjoitukset/app/src/main/java/com/example/harjoitus_01/ProgrammingTechnology.kt
@@ -0,0 +1,9 @@
+package com.example.harjoitus_01
+
+data class ProgrammingTechnology(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val experienceYears: Double,
+    val isFavorite: Boolean,
+)

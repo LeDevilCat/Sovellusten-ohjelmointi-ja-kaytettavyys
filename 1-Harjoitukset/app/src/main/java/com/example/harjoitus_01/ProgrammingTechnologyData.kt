58aaee44375f4d5c3510acabf734fa89b2ae7a25
Jyri Nieminen <nieminen.jyri.j@gmail.com>
2026-09-23 18:56:59 +0300

    5th Assingment, part 2
    Created programmingTechnologyData.kt to place example data in

---
+++ 1-Harjoitukset/app/src/main/java/com/example/harjoitus_01/ProgrammingTechnologyData.kt
@@ -0,0 +1,60 @@
+package com.example.harjoitus_01
+
+val ProgrammingTechnologies = listOf(
+    ProgrammingTechnology(
+        id = 1,
+        name = "Kotlin",
+        description = "Moderni ohjelmointikieli android kehitykseen.",
+        experienceYears = 0.08,
+        isFavorite = false
+    ),
+    ProgrammingTechnology(
+        id = 2,
+        name = "Java",
+        description = "Ohjelmointikieli, jota käytetään esimerkiksi palvelin- ja Android-kehityksessä",
+        experienceYears = 0.16,
+        isFavorite = false
+    ),
+    ProgrammingTechnology(
+        id = 3,
+        name = "C++",
+        description = "Ohjelmointikieli, jota käytetään esimerkiksi järjestelmä- ja sovelluskehityksessä",
+        experienceYears = 1.5,
+        isFavorite = false
+    ),
+    ProgrammingTechnology(
+        id = 4,
+        name = "Python",
+        description = "Helppokäyttöinen ohjelmointikieli esimerkiksi automaatioon ja datankäsittelyyn",
+        experienceYears = 0.5,
+        isFavorite = false
+    ),
+    ProgrammingTechnology(
+        id = 5,
+        name = "HTML & CSS",
+        description = "Web-sivujen rakenteen ja ulkoasun määrittelyyn käytettävät teknologiat",
+        experienceYears = 6.0,
+        isFavorite = true
+    ),
+    ProgrammingTechnology(
+        id = 6,
+        name = "JavaScript",
+        description = "Web-kehityksessä laajasti käytetty ohjelmointikieli",
+        experienceYears = 2.0,
+        isFavorite = true
+    ),
+    ProgrammingTechnology(
+        id = 7,
+        name = "TypeScript",
+        description = "JavaScriptiin perustuva tyypitetty ohjelmointikieli web-sovellusten kehittämiseen",
+        experienceYears = 0.04,
+        isFavorite = true
+    ),
+    ProgrammingTechnology(
+        id = 8,
+        name = "React",
+        description = "JavaScript-kirjasto käyttöliittymien ja yksisivuisten web-sovellusten rakentamiseen",
+        experienceYears = 0.24,
+        isFavorite = true
+    ),
+)
